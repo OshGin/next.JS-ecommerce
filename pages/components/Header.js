6c08df90d29887cc61d7520d93b5b1f8b0b66fcc
Mathias Angerer <>
2022-02-02 09:01:12 +0100

    Set up page

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,7 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
 export default function Header() {
   return (
     <header>
+      <Image
+        src="/logo.png"
+        alt="Logo for Click and buy"
+        width={72}
+        height={72}
+      />
       <Link href="/">
         <a> Home</a>
       </Link>
