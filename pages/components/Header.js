a2ae6e06b0e7d5677d64f44dd4463f38c6da6281
Mathias Angerer <>
2022-02-03 13:52:08 +0100

    Add emotion

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,8 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { css } from '@emotion/react';
+
+const headeStyles = css`
+  background-color: #83eb83;
+  padding: 10px 15 px;
+  border-radius: 4px;
+  margin-bottom: 20px;
+  font-size: 2rem;
+`;
+
 export default function Header() {
   return (
-    <header>
+    <header css={headeStyles}>
       <Image
         src="/logo.png"
         alt="Logo for Click and buy"
--- pages/products.js
+++ pages/products.js
@@ -1,8 +1,20 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 export default function ProductOverview() {
   return (
     <div>
-      <h1> Product Overview </h1>
-      <p>All you can click on!</p>
+      <title>click and buy</title>
+      <meta name="ecommerce picture store" content="click and buy" />
+      <link rel="icon" href="/favicon.ico" />
+      <Header />
+      <main>
+        {' '}
+        <h1> Product Overview </h1>
+        <p>All you can click on!</p>
+        <p> Hear you will find the products soon!</p>
+      </main>
+      <Footer />
     </div>
   );
 }
